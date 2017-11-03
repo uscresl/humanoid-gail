6f8e3bdb64a65f15d5e1b72e98aab35da581619f
Eric Heiden <eric@eric-heiden.de>
2017-11-03 04:34:26 +0000

    Rendering support without hacking baselines

--- rl/run_rl.py
+++ rl/run_rl.py
@@ -3,7 +3,6 @@
 import mujoco_py # Mujoco must come before other imports. https://openai.slack.com/archives/C1H6P3R7B/p1492828680631850
 from mpi4py import MPI
 from baselines.common import set_global_seeds
-import os.path as osp
 import gym
 import logging
 import tensorflow as tf
@@ -12,9 +11,8 @@ from baselines.ppo1.mlp_policy import MlpPolicy
 from baselines.common.mpi_fork import mpi_fork
 # from baselines import bench
 from baselines.trpo_mpi import trpo_mpi
-from baselines.trpo_mpi.trpo_mpi import traj_segment_generator
 import baselines.common.tf_util as U
-import sys
+import numpy as np
 
 import mocap_env.gym_mocap_walker
 import mocap_env.gym_mujoco_walker
@@ -39,6 +37,65 @@ def learning_iteration(locals, globals):
     iteration += 1
     if iteration % 20 == 0:
         save()
+
+
+def traj_segment_generator(pi, env, horizon, stochastic, human_render=False):
+    # Initialize state variables
+    t = 0
+    ac = env.action_space.sample()
+    new = True
+    rew = 0.0
+    ob = env.reset()
+
+    cur_ep_ret = 0
+    cur_ep_len = 0
+    ep_rets = []
+    ep_lens = []
+
+    # Initialize history arrays
+    obs = np.array([ob for _ in range(horizon)])
+    rews = np.zeros(horizon, 'float32')
+    vpreds = np.zeros(horizon, 'float32')
+    news = np.zeros(horizon, 'int32')
+    acs = np.array([ac for _ in range(horizon)])
+    prevacs = acs.copy()
+
+    while True:
+        prevac = ac
+        ac, vpred = pi.act(stochastic, ob)
+        # Slight weirdness here because we need value function at time T
+        # before returning segment [0, T-1] so we get the correct
+        # terminal value
+        if t > 0 and t % horizon == 0:
+            yield {"ob" : obs, "rew" : rews, "vpred" : vpreds, "new" : news,
+                    "ac" : acs, "prevac" : prevacs, "nextvpred": vpred * (1 - new),
+                    "ep_rets" : ep_rets, "ep_lens" : ep_lens}
+            _, vpred = pi.act(stochastic, ob)
+            # Be careful!!! if you change the downstream algorithm to aggregate
+            # several of these batches, then be sure to do a deepcopy
+            ep_rets = []
+            ep_lens = []
+        i = t % horizon
+        obs[i] = ob
+        vpreds[i] = vpred
+        news[i] = new
+        acs[i] = ac
+        prevacs[i] = prevac
+
+        ob, rew, new, _ = env.step(ac)
+        rews[i] = rew
+        if human_render:
+            env.render()
+
+        cur_ep_ret += rew
+        cur_ep_len += 1
+        if new:
+            ep_rets.append(cur_ep_ret)
+            ep_lens.append(cur_ep_len)
+            cur_ep_ret = 0
+            cur_ep_len = 0
+            ob = env.reset()
+        t += 1
 
 
 def train(env_id, num_timesteps, seed):
